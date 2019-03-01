f3c9edbb79d0a9c359eedb40fa5d71fcc352daae
lopezfjose <jflopezfernandez@gmail.com>
2019-03-01 00:40:31 -0500

    Set up module and successfully imported into test project.

--- Bio/Bio.pm
+++ Bio/Bio.pm
@@ -1,11 +1,26 @@
 #!/usr/bin/perl
+package Bio;
+
 use strict;
 use warnings;
 
-sub Initialized {
-    print "Bio library initialized...\n";
+BEGIN {
+    require Exporter;
 
-    return;
+    # Set the version for version checking.
+    our $Version = 1.00;
+
+    # Inherit from Exporter to export functions and variables.
+    our @ISA = qw(Exporter);
+
+    # Functions and variables which are exported by default.
+    our @EXPORT = qw();
+
+    # Functions and variables which can be optionally exported.
+    our @EXPORT_OK = qw();
 }
 
+# TODO: Add subroutines.
+
+# Mandatory true return value.
 1;
